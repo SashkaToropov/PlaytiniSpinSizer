974b0ddc7f0fd842dd72b805083e31c4034f339f
SashkaToropov <toropov.oleksandr03@gmail.com>
2024-03-14 13:31:39 +0200

    Created ball and set its rotation

--- PlaytiniSpinSizer/ViewController.swift
+++ PlaytiniSpinSizer/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var circleSize = 70.0
+    
+    private lazy var ballImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.frame = CGRect(x: 0, y: 0, width: circleSize, height: circleSize)
+        imageView.center = view.center
+        imageView.image = UIImage(named: "ball")
+        imageView.layer.cornerRadius = circleSize / 2
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.addSubview(ballImageView)
+        
+        startBallRotation()
     }
-
-
+    
+    private func startBallRotation() {
+        UIView.animate(withDuration: 2.0, delay: 0.0, options: [.repeat, .curveLinear]) {
+            self.ballImageView.transform = CGAffineTransform(rotationAngle: .pi)
+        }
+    }
 }
 
